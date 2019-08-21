e1e7238062f5fd73c2c387fa8ff9978b39a3d7b9
Philip Daugherty <Lord.Agion@gmail.com>
2019-08-21 15:52:18 -0400

    yes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+//button 1
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("#text1").toggle();
+    });
+});
